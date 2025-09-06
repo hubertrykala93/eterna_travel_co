043b14394a8dac6ea597aacfc5cdbdd5b5da9bee
hubertrykala93 <hubert.rykala@gmail.com>
2025-09-06 14:04:32 +0200

    Style Contact Us Page

--- frontend/apps/eterna_travel_co/src/app/app.ts
+++ frontend/apps/eterna_travel_co/src/app/app.ts
@@ -1,15 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { ButtonComponent } from '@shared/ui';
 import { HeaderComponent } from './core/header/header.component';
 import { HeroComponent } from './core/hero/hero.component';
 
 @Component({
-  imports: [RouterModule, HeaderComponent, HeroComponent, ButtonComponent],
+  imports: [RouterModule, HeaderComponent, HeroComponent],
   selector: 'et-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
-  standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class App {}
