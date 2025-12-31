bc0f2e3511597545f7150511c5ca11012ca748f8
hubertrykala93 <hubert.rykala@gmail.com>
2025-12-31 17:02:41 +0100

    Send Request for Refresh Token

--- frontend/libs/user/data-access/src/lib/user.initializer.ts
+++ frontend/libs/user/data-access/src/lib/user.initializer.ts
@@ -1,16 +1,31 @@
-import { catchError, of, tap } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
+import { catchError, of, switchMap, tap } from 'rxjs';
+import { AuthenticationService } from './authentication.service';
 import { UserService } from './user.service';
 import { UserStoreInstance } from './user.store';
 
-export const userInitializer = (userService: UserService, userStore: UserStoreInstance) => {
+export const userInitializer = (
+  userService: UserService,
+  authenticationService: AuthenticationService,
+  userStore: UserStoreInstance,
+) => {
   return userService.getCurrentUser().pipe(
     tap((user) => {
-      console.log('User -> ', user);
       userStore.setUser(user);
     }),
-    catchError((error) => {
-      console.log('Error -> ', error);
+    catchError((error: HttpErrorResponse) => {
+      if (error.status === 401) {
+        return authenticationService
+          .token()
+          .pipe(
+            switchMap(() =>
+              userService.getCurrentUser().pipe(tap((user) => userStore.setUser(user))),
+            ),
+          );
+      }
+
       userStore.setUser(null);
+
       return of(null);
     }),
   );
--- frontend/libs/user/data-access/src/lib/user.provider.ts
+++ frontend/libs/user/data-access/src/lib/user.provider.ts
@@ -4,12 +4,15 @@ import {
   makeEnvironmentProviders,
   provideAppInitializer,
 } from '@angular/core';
+import { AuthenticationService } from './authentication.service';
 import { userInitializer } from './user.initializer';
 import { UserService } from './user.service';
 import { UserStore } from './user.store';
 
 export function provideUser(): EnvironmentProviders {
   return makeEnvironmentProviders([
-    provideAppInitializer(() => userInitializer(inject(UserService), inject(UserStore))),
+    provideAppInitializer(() =>
+      userInitializer(inject(UserService), inject(AuthenticationService), inject(UserStore)),
+    ),
   ]);
 }
